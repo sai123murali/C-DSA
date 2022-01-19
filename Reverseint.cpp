45357487874548c83d65050d0bf0e2f5b12c3fdd
sai123mural <sai123murali@gmail.com>
2022-01-19 16:01:21 +0530

    sasa

--- Reverseint.cpp
+++ Reverseint.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 
 int main(){
-    
     int x;
     cin>>x;
-    int rev=0,answer=0;
+      int answer=0;
         while(x!=0){
             int rem=x%10;
-            answer=answer*10+rem;
-             x=x/10;      
-           
-
+            if((answer>INT_MAX/10) ||(answer<INT_MIN/10) )
+            {
+                return 0;          
+            }
+            answer=answer*10+rem;  
+            x=x/10;
+            
         }
-        cout<<answer;
+        cout<< answer;
 
 }
