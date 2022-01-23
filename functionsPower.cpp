9466b5480d4e149fbc52de696543edc40d1cae95
sai123mural <sai123murali@gmail.com>
2022-01-23 20:50:21 +0530

    ch

--- functionsPower.cpp
+++ functionsPower.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int power(int a, int b){
 
+int a,b;
+
+cin>>a>>b;
+cout<<endl;
+
     int ans=1;
 
     for(int i=1;i<=b;i++){
@@ -13,11 +18,6 @@ int power(int a, int b){
 }
 
 int main(){
-
-int a,b;
-
-cin>>a>>b;
-cout<<endl;
 
 int answer=power(a,b);
 cout<<" The power of "<<a <<" "<<b<<" "<<"is:"<<answer;
