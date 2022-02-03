d0cf730204a70a0293cc26b8fb2327a55e32b023
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2022-02-03 16:00:39 +0530

    Create intersection.cpp
    ds

---
+++ intersection.cpp
@@ -0,0 +1,16 @@
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	// Write your code here.
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        int element=arr1[i];
+        for(int j=0;j<m;j++){
+            if(element==arr2[j]){
+                ans.push_back(element);
+                arr2[j]=-1;
+                break;
+            }
+        }
+    }
+    return ans;
+}
